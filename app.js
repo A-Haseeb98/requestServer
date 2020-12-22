56a4c9341fd679e3ab874a1b5e114c1f4aef469a
A-Haseeb98 <76472496+A-Haseeb98@users.noreply.github.com>
2020-12-22 17:45:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+function get() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://192.168.50.177:3000/bulb';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        document.write(Http.responseText);
+    }
+}
+
+function post() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://192.168.50.177:3000/bulb/';
+    Http.open("POST", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
+
+function put() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://192.168.50.177:3000/bulb/';
+    Http.open("PUT", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
+
+function dlf() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://192.168.50.177:3000/bulb/';
+    Http.open("DELETE", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
